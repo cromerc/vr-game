2ba638076a9595f2efcc0f199ad6131c2be94c6c
Chris Cromer <chris@cromer.cl>
2022-12-17 23:39:56 -0300

    add death scene

--- Assets/Scripts/Boo.cs
+++ Assets/Scripts/Boo.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Boo : MonoBehaviour
 {
@@ -179,6 +180,14 @@ public class Boo : MonoBehaviour
                 _previousDirection = _direction;
                 _direction = Direction.None;
             }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            SceneManager.LoadScene("Dead");
         }
     }
 
